0ecdb8d0d32baac6ab4bcd88a99354946177dd0e
Kimberly Townsend <iamkimbert@gmail.com>
2022-09-01 14:43:39 -0400

    Initial Commit

---
+++ Custom_Side_Menu/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Custom_Side_Menu
+//
+//  Created by Kimberly Townsend on 9/1/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Custom_Side_Menu/Custom_Side_MenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  Custom_Side_MenuApp.swift
+//  Custom_Side_Menu
+//
+//  Created by Kimberly Townsend on 9/1/22.
+//
+
+import SwiftUI
+
+@main
+struct Custom_Side_MenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
